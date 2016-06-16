b875774487a43dcb088910c9cf4a9f09a94bd933
Justin Clark-Casey <justincc@intermine.org>
2016-06-16 16:48:00 +0100

    convert tabs to spaces in SynbioGFF3RecordHandler.java

--- bio/sources/synbio/synbio-gff/main/src/org/intermine/bio/dataconversion/SynbioGFF3RecordHandler.java
+++ bio/sources/synbio/synbio-gff/main/src/org/intermine/bio/dataconversion/SynbioGFF3RecordHandler.java
@@ -29,121 +29,121 @@ public class SynbioGFF3RecordHandler extends GFF3RecordHandler
 {
   private static final Logger LOG = Logger.getLogger(SynbioGFF3RecordHandler.class);
 
-	private Map<String, String> geneIdToPrimaryIdentifier = new HashMap<String, String>();
-	
-	/**
-	 * Create a new SynbioGffGFF3RecordHandler for the given data model.
-	 * @param model the model for which items will be created
-	 */
-	public SynbioGFF3RecordHandler (Model model) {
-		super(model);		
-		geneIdToPrimaryIdentifier = new HashMap<String, String>();
-	}
+    private Map<String, String> geneIdToPrimaryIdentifier = new HashMap<String, String>();
+    
+    /**
+     * Create a new SynbioGffGFF3RecordHandler for the given data model.
+     * @param model the model for which items will be created
+     */
+    public SynbioGFF3RecordHandler (Model model) {
+        super(model);        
+        geneIdToPrimaryIdentifier = new HashMap<String, String>();
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void process(GFF3Record record) {
-		// This method is called for every line of GFF3 file(s) being read.  Features and their
-		// locations are already created but not stored so you can make changes here.  Attributes
-		// are from the last column of the file are available in a map with the attribute name as
-		// the key.   For example:
-		//
-		//     Item feature = getFeature();
-		//     String symbol = record.getAttributes().get("symbol");
-		//     feature.setAttrinte("symbol", symbol);
-		//
-		// Any new Items created can be stored by calling addItem().  For example:
-		// 
-		//     String geneIdentifier = record.getAttributes().get("gene");
-		//     gene = createItem("Gene");
-		//     gene.setAttribute("primaryIdentifier", geneIdentifier);
-		//     addItem(gene);
-		//
-		// You should make sure that new Items you create are unique, i.e. by storing in a map by
-		// some identifier. 
-	
-		Item feature = getFeature();
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void process(GFF3Record record) {
+        // This method is called for every line of GFF3 file(s) being read.  Features and their
+        // locations are already created but not stored so you can make changes here.  Attributes
+        // are from the last column of the file are available in a map with the attribute name as
+        // the key.   For example:
+        //
+        //     Item feature = getFeature();
+        //     String symbol = record.getAttributes().get("symbol");
+        //     feature.setAttrinte("symbol", symbol);
+        //
+        // Any new Items created can be stored by calling addItem().  For example:
+        // 
+        //     String geneIdentifier = record.getAttributes().get("gene");
+        //     gene = createItem("Gene");
+        //     gene.setAttribute("primaryIdentifier", geneIdentifier);
+        //     addItem(gene);
+        //
+        // You should make sure that new Items you create are unique, i.e. by storing in a map by
+        // some identifier. 
+    
+        Item feature = getFeature();
 
-		// going to be either gene or CDS
-		String type = record.getType();
-		
-		if ("gene".equals(type)) {
-			
-			String id = record.getId();
-			geneIdToPrimaryIdentifier.put(id, feature.getIdentifier());
-			
-			boolean addedSecondaryIdentifier = false;
+        // going to be either gene or CDS
+        String type = record.getType();
+        
+        if ("gene".equals(type)) {
+            
+            String id = record.getId();
+            geneIdToPrimaryIdentifier.put(id, feature.getIdentifier());
+            
+            boolean addedSecondaryIdentifier = false;
 
-			List<String> xrefs = record.getDbxrefs();
-			if (xrefs != null && !xrefs.isEmpty()) {
-				Iterator<String> it = xrefs.iterator();
-				while (it.hasNext()) {
-					String xref = it.next();
-					String[] bits = xref.split(":");
-					if (bits != null && bits.length == 2) {
-						String db = bits[0];
-						String secondaryIdentifier = bits[1];
-						if ("EcoGene".equals(db)) {
-							feature.setAttribute("secondaryIdentifier", secondaryIdentifier);
-							addedSecondaryIdentifier = true;
-						}
-					}
-				}
-			}
+            List<String> xrefs = record.getDbxrefs();
+            if (xrefs != null && !xrefs.isEmpty()) {
+                Iterator<String> it = xrefs.iterator();
+                while (it.hasNext()) {
+                    String xref = it.next();
+                    String[] bits = xref.split(":");
+                    if (bits != null && bits.length == 2) {
+                        String db = bits[0];
+                        String secondaryIdentifier = bits[1];
+                        if ("EcoGene".equals(db)) {
+                            feature.setAttribute("secondaryIdentifier", secondaryIdentifier);
+                            addedSecondaryIdentifier = true;
+                        }
+                    }
+                }
+            }
 
-			if (!addedSecondaryIdentifier) {
-			    List<String> locus = record.getAttributes().get("old_locus_tag");
-	            if (locus != null && !locus.isEmpty()) {
-	                Iterator<String> it = locus.iterator();
-	                String identifier = it.next();
-	                feature.setAttribute("secondaryIdentifier", identifier);
-	            }			    
-			}			
+            if (!addedSecondaryIdentifier) {
+                List<String> locus = record.getAttributes().get("old_locus_tag");
+                if (locus != null && !locus.isEmpty()) {
+                    Iterator<String> it = locus.iterator();
+                    String identifier = it.next();
+                    feature.setAttribute("secondaryIdentifier", identifier);
+                }                
+            }            
 
-			// no dbxref=EcoGene, use locus
-			List<String> locus = record.getAttributes().get("locus_tag");
-			if (locus != null && !locus.isEmpty()) {
-				Iterator<String> it = locus.iterator();
-				String primaryIdentifier = it.next();
-				feature.setAttribute("primaryIdentifier", primaryIdentifier);
-			} else {
-				final String errorMessage = "Could not find primary identifier for " 
-						+ record.getId();
-				throw new BuildException(errorMessage);
-			}
+            // no dbxref=EcoGene, use locus
+            List<String> locus = record.getAttributes().get("locus_tag");
+            if (locus != null && !locus.isEmpty()) {
+                Iterator<String> it = locus.iterator();
+                String primaryIdentifier = it.next();
+                feature.setAttribute("primaryIdentifier", primaryIdentifier);
+            } else {
+                final String errorMessage = "Could not find primary identifier for " 
+                        + record.getId();
+                throw new BuildException(errorMessage);
+            }
 
-			// name
-			List<String> names = record.getNames();
-			if (names != null && !names.isEmpty()) {
-				Iterator<String> it = names.iterator();
-				String name = it.next();
-				feature.setAttribute("symbol", name);
-			}
+            // name
+            List<String> names = record.getNames();
+            if (names != null && !names.isEmpty()) {
+                Iterator<String> it = names.iterator();
+                String name = it.next();
+                feature.setAttribute("symbol", name);
+            }
 
-//			List<String> synonyms = record.getAttributes().get("gene_synonym");
-//			if (synonyms != null && !synonyms.isEmpty()) {
-//				Iterator<String> it = synonyms.iterator();
-//				while(it.hasNext()) {
-//					String synonymValue = it.next();
-//					Item synonym = converter.createItem("Synonym");
-//					synonym.setAttribute("value", synonymValue);
-//					feature.addToCollection("synonyms", synonym);
-//					addItem(synonym);
-//				}
-//			}
+//            List<String> synonyms = record.getAttributes().get("gene_synonym");
+//            if (synonyms != null && !synonyms.isEmpty()) {
+//                Iterator<String> it = synonyms.iterator();
+//                while(it.hasNext()) {
+//                    String synonymValue = it.next();
+//                    Item synonym = converter.createItem("Synonym");
+//                    synonym.setAttribute("value", synonymValue);
+//                    feature.addToCollection("synonyms", synonym);
+//                    addItem(synonym);
+//                }
+//            }
 
 
-		} else if ("CDS".equals(type)) {
-			List<String> names = record.getAttributes().get("Name");
-			if (names != null && !names.isEmpty()) {
-				Iterator<String> it = names.iterator();
-				String primaryIdentifier = it.next(); 
-				feature.setAttribute("primaryIdentifier", primaryIdentifier);
-			}
-			
-			List<String> parents = record.getParents();
+        } else if ("CDS".equals(type)) {
+            List<String> names = record.getAttributes().get("Name");
+            if (names != null && !names.isEmpty()) {
+                Iterator<String> it = names.iterator();
+                String primaryIdentifier = it.next(); 
+                feature.setAttribute("primaryIdentifier", primaryIdentifier);
+            }
+            
+            List<String> parents = record.getParents();
 
       if (parents != null) {
           Iterator<String> it = parents.iterator();
@@ -156,6 +156,6 @@ public class SynbioGFF3RecordHandler extends GFF3RecordHandler
       } else {
           LOG.warn("Couldn't set gene parent feature on CDS (" + record.getId() + "," + feature.getAttribute("primaryIdentifier") + ") since no Parent attribute found");
       }
-		}
-	}
+        }
+    }
 }
